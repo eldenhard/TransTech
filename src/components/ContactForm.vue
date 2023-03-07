d766bf7354a7e5fb9639a8eb00592ccfa571d861
eldenhard <reshetilo-1997@mail.ru>
2023-03-07 14:19:55 +0300

    подвал, тексты, моадльное окно

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -2,14 +2,14 @@
     <div>
         <div class="row mr-0" style="background: #171717" id="contact">
             <div class="col-md-6 mr-0" >
-                <img src="@/assets/map.png" alt="" style="width:90%; height: 80%; margin-left: 7%; margin-top: 7%;">>
+                <img src="@/assets/map.png" alt="" style="width:90%; height: 80%; margin-left: 7%; margin-top: 7%;">
             </div>
             <div class="col-md-6 mr-0 right-contact" >
                     <h2 style="padding-left: 7%;"><span style="border-bottom: 3px solid #EB272B;">Контакты</span></h2>
-                    <h4 style="padding-left: 7%; padding-top: 2%;"> Тел. +7 (495) 363 1520</h4>
+                    <h4 style="padding-left: 7%; padding-top: 2%;">Тел. <a href="tel:+7 (495)363-15-20" style="color: white; outline: none !important;">+7 (495) 363-15-20</a></h4>
                     <h4 style="padding-left: 7%; padding-top: 2%;">E-mail: <span style="color: #ff5154;">info@tehtrans.com</span></h4>
                     <p style="padding-left: 7%; padding-top: 2%;"> 143026, Московская область, г.о. Одинцовский, рп Новоивановское, шоссе Можайское, влд. 165, БЦ «Premium West»</p>
-                <p style="padding-left: 7%; padding-top: 2%;">Дополнительный филиал:
+                <p style="padding-left: 7%; padding-top: 2%;">Дополнительный офис:
                 121165, Москва, Кутузовский проспект, 30</p>
         </div>
     </div>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,23 +1,159 @@
 <template>
-        <footer>
+    <div>
+
+
+    <b-modal id="bv-modal-example" hide-footer size="xl">
+    <template #modal-title>
+        Оценка условий труда
+    </template>
+    <div class="d-block text-center fsize">
+          
+        <b-carousel
+      id="mainCarousel"
+      v-model="slide"
+      :interval="7000"
+      controls
+      indicators
+      style="background: grey;"
+      @sliding-start="onSlideStart"
+      @sliding-end="onSlideEnd"
+    >
+      <!-- Text slides with image -->
+      <b-carousel-slide>
+        <template #img>
+            <div class="item-photo">
+                <img
+                    class="d-block img-fluid w-100"
+                    src="../assets/1.jpg"
+                    alt="image slot"
+                >
+            </div>
+
+        </template>
+      </b-carousel-slide>
+
+      <!-- Slides with custom text -->
+      <b-carousel-slide>
+        <template #img>
+            <div class="item-photo">
+                    <img
+                        class="d-block img-fluid w-100"
+                        src="../assets/2.jpg"
+                        alt="image slot"
+                    >
+        </div>
+        </template>
+      </b-carousel-slide>
+
+      <!-- Slides with image only -->
+      <b-carousel-slide>
+        <template #img>
+            <div class="item-photo">
+                    <img
+                        class="d-block img-fluid w-100"
+                        src="../assets/3.jpg"
+                        alt="image slot"
+                    >
+        </div>
+        </template>
+      </b-carousel-slide>
+
+      <!-- Slides with img slot -->
+      <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
+      <b-carousel-slide>
+        <template #img>
+            <div class="item-photo">
+                    <img
+                        class="d-block img-fluid w-100"
+                        src="../assets/4.jpg"
+                        alt="image slot"
+                    >
+        </div>
+        </template>
+      </b-carousel-slide>
+      <b-carousel-slide>
+        <template #img>
+            <div class="item-photo">
+                    <img
+                        class="d-block img-fluid w-100"
+                        src="../assets/5.jpg"
+                        alt="image slot"
+                    >
+        </div>
+        </template>
+      </b-carousel-slide>
+      <b-carousel-slide>
+        <template #img>
+            <div class="item-photo">
+                    <img
+                        class="d-block img-fluid w-100"
+                        src="../assets/6.jpg"
+                        alt="image slot"
+                    >
+        </div>
+        </template>
+      </b-carousel-slide>
+
+    </b-carousel>
+    </div>
+    <b-button class="mt-3" block @click="$bvModal.hide('bv-modal-example')" style="background: #EB272B; border: none;">Закрыть</b-button>
+  </b-modal>
+
+   
+        <footer class="mainFooter">
             <div class="parent">
             <img src="@/assets/photo.png" alt="" class="footer-logo">
-            <p class="safety-inf">© 2017-2023 ООО «Транспортные Технологии». Все права защищены.</p>
-            <p class="contact-phone">+7 (495) 363 1520</p>
+            <div>
+                <p class="safety-inf">© 2017-2023 ООО «Транспортные Технологии». Все права защищены. <br>
+                    <button id="show-btn" @click="$bvModal.show('bv-modal-example')" style="padding: 0; font-size: 12px; background: transparent; border: none;  color: white;">Документы об оценке условий труда </button>
+
+                </p>
+            </div>
+            <a class="contact-phone"  href="tel:+7 (495)363-15-20">+7 (495)363-15-20</a>
         </div>
 
         </footer>
-
+    </div>
 </template>
 
 <script>
 export default{
-    name: 'footer'
-}
+    name: 'footer',
+    data() {
+      return {
+        slide: 0,
+        sliding: null
+      }
+    },
+    methods: {
+      onSlideStart(slide) {
+        this.sliding = true
+      },
+      onSlideEnd(slide) {
+        this.sliding = false
+      }
+    }
+  }
+
 </script>
 
-<style>
-footer{
+<style scoped>
+.fsize{
+    min-height: 80vh !important;
+     max-height: 80vh !important;
+}
+#show-btn:hover{
+    outline: white !important;
+    border-bottom: 1px solid rgb(218, 218, 218) !important;
+    color: rgb(218, 218, 218) !important;
+}
+.item-photo img{
+    filter: brightness(90%);
+    height: 700px;
+    width: 100%;
+}
+
+.mainFooter{
     width: 100%;
     background: #565656;
 } 
@@ -25,6 +161,7 @@ footer{
 width: 12%;
 height: 45%;
 padding-left: 1%;
+margin-bottom: 1%;
 }
 .parent{
     display: flex;
@@ -36,12 +173,26 @@ padding-left: 1%;
 .parent p{
     font-weight: 600;
 }
+.contact-phone {
+    color: white !important;
+    outline: none !important;
+    border-bottom: none !important;
+}
 
-@media screen and (max-width: 500px) {
+@media screen and (max-width: 700px) {
 .footer-logo {
     /* width: 50%; */
     /* height: 10%; */
     display: none;
+}
+.item-photo img{
+    filter: brightness(90%);
+    /* height: 100%;
+    width: 100%; */
+}
+.d-block{
+    min-height: 20vh !important;
+    max-height: 20vh !important;
 }
 .safety-inf {
     font-size: 9px;
@@ -53,6 +204,13 @@ padding-left: 1%;
 }
 footer{
     height: 40px;
+}
+#show-btn{
+    position: relative !important;
+    left:50% !important;
+    transform: translate(-50%,0) !important;
+    text-align: center !important;
+    float: left;
 }
 }
 </style>
